8bcc1d37bb1f2d577caf6daccdc31eca6904973b
Karan-symbi <karan.chauahn.btech2023@sitpune.edu.in>
2025-02-03 21:30:09 +0530

    Update ArrayInput.java

--- ArrayInput.java
+++ ArrayInput.java
@@ -1,3 +1,14 @@
 //ArrayInput.java
 import java.util.*;
 class ArrayInput{
+	 int[] userInput(){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please enter the length of the array:");
+		int n = sc.nextInt();
+		int[] array = new int[n];
+		System.out.println("Enter" + n +  "numbers:");
+		for(int i = 0;i<n;i++){
+			array[i] = sc.nextInt();
+		}
+		return array;	
+	}
