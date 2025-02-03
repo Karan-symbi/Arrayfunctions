d476426a0f48eb714c26c0ef71bae9f937da03d6
Karan-symbi <karan.chauahn.btech2023@sitpune.edu.in>
2025-02-03 21:31:40 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,3 +5,9 @@ Karan Chauhan
 23070126053
 AIML A3
 */
+class Main{
+	public static void main(String[] args){
+		ArrayInput ai = new ArrayInput();
+		ArrayFunctions af = new ArrayFunctions();
+		Scanner sc = new Scanner(System.in);
+		int choice;
