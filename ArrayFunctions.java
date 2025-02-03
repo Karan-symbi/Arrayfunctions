4f8ee7630df3d806f1da9ee1c7419b6a08dcdce5
Karan-symbi <karan.chauahn.btech2023@sitpune.edu.in>
2025-02-03 21:24:53 +0530

    Update ArrayFunctions.java

--- ArrayFunctions.java
+++ ArrayFunctions.java
@@ -4,3 +4,13 @@ class ArrayFunctions{
 	void display(int[] array){
 		System.out.println("Array is:" + Arrays.toString(array));
 	}
+	void OddEven(int[] array){
+		int[] even = new int[5];
+		int[] odd = new int[5];
+		int odd_index =0;
+		int even_index = 0;
+		for(int i=0;i<array.length;i++){
+			if(array[i]%2==0){
+				even[even_index] = array[i];
+				even_index++;		
+			}
