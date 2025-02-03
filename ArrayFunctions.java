31e4da54eea1ece994c1e29da17684a9d2ce35e8
Karan-symbi <karan.chauahn.btech2023@sitpune.edu.in>
2025-02-03 21:26:12 +0530

    Update ArrayFunctions.java

--- ArrayFunctions.java
+++ ArrayFunctions.java
@@ -25,3 +25,16 @@ class ArrayFunctions{
 	void displayList(ArrayList<Integer> array){
 		System.out.println("Array is:" + array);
 	}
+	void OddEvenList(int[] array){
+		ArrayList<Integer> even = new ArrayList<Integer>();
+		ArrayList<Integer> odd = new ArrayList<Integer>();
+
+		for(int i=0;i<array.length;i++){
+			if(array[i]%2==0){
+				even.add(array[i]);
+			
+			}
+			else{
+				odd.add(array[i]);
+					
+			}
